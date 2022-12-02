b1bbd5ee58470897c142cef6342af36197bcaa9c
wizba <33273335+wizba@users.noreply.github.com>
2022-12-02 07:13:36 +0200

    created a list memes future and navigate to meme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import MimeList from './ListComponent/MimeList';
 
 export default function App() {
+  let after = '';
+  const [listItems, setListItems] = useState(null);
+  const fetchData = async () => {
+    let url = 'https://www.reddit.com/r/aww.json';
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((body) => {
+        const { data } = body;
+        console.log(data);
+        const { children } = data;
+
+        console.log(children.map((val) => val.data));
+
+        setListItems(children.map((val) => val.data));
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <div class="_row">
-  <div class="column">ONE</div>
-  <div class="column">TWO</div>
-  <div class="column">THREE</div>
-  <div class="column">FOUR</div>
-  <div class="column">FIVE</div>
-  <div class="column">SIX</div>
-  <div class="column">SEVEN</div>
-  <div class="column">EIGHT</div>
-  <div class="column">NINE</div>
-  <div class="column">TEN</div>
-  <div class="column">ELEVEN</div>
-  <div class="column">TWELVE</div>
-</div>
+
+      <MimeList fetchData={fetchData} listItems={listItems} />
     </div>
   );
 }
