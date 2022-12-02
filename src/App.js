c32be87188cb0358b7f5e21eaaa2e1fa8a12898c
wizba <33273335+wizba@users.noreply.github.com>
2022-12-02 04:47:24 +0200

    Added grid style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <div class="_row">
+  <div class="column">ONE</div>
+  <div class="column">TWO</div>
+  <div class="column">THREE</div>
+  <div class="column">FOUR</div>
+  <div class="column">FIVE</div>
+  <div class="column">SIX</div>
+  <div class="column">SEVEN</div>
+  <div class="column">EIGHT</div>
+  <div class="column">NINE</div>
+  <div class="column">TEN</div>
+  <div class="column">ELEVEN</div>
+  <div class="column">TWELVE</div>
+</div>
     </div>
   );
 }
